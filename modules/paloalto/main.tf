24a5370f33b32fc711b1650f05e87bdceeed9653
Joyce Mwangi <44641045+joycemwangi@users.noreply.github.com>
2025-11-20 15:08:41 +0300

    Rename resource from paloalto_address_object to panos_address_object

--- modules/paloalto/main.tf
+++ modules/paloalto/main.tf
@@ -1,7 +1,7 @@
-resource "paloalto_address_object" "core_firewall_obj" {
+resource "panos_address_object" "core_firewall_obj" {
   name        = var.object_name
   value       = var.object_value
   type        = var.object_type
-  tag         = var.tags
+  tags        = var.tags
   description = "Managed by Terraform"
 }
