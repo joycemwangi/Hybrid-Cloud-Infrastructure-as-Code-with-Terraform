9b6281dc797a57e3bcf60f3ea1250ea2ed427e29
Joyce Mwangi <44641045+joycemwangi@users.noreply.github.com>
2025-11-20 04:37:49 +0300

    Add type and tag to paloalto_address_object

--- modules/paloalto/main.tf
+++ modules/paloalto/main.tf
@@ -1,5 +1,7 @@
 resource "paloalto_address_object" "core_firewall_obj" {
   name        = var.object_name
   value       = var.object_value
+  type        = var.object_type
+  tag         = var.tags
   description = "Managed by Terraform"
 }
