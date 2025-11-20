0948117f4a1cd0f2ed10acc79b9c9d0ef2b32f3e
Joyce Mwangi <44641045+joycemwangi@users.noreply.github.com>
2025-11-20 04:37:09 +0300

    Update variables.tf

--- modules/paloalto/variables.tf
+++ modules/paloalto/variables.tf
@@ -1,2 +1,19 @@
-variable "object_name" {}
-variable "object_value" {}
+variable "object_name" {
+  type = string
+}
+
+variable "object_value" {
+  type = string
+}
+
+# NEW — Address object type (ip-netmask, ip-range, fqdn)
+variable "object_type" {
+  type    = string
+  default = "ip-netmask"
+}
+
+# NEW — Tags to attach to the Palo Alto address object
+variable "tags" {
+  type    = list(string)
+  default = ["terraform-managed"]
+}
