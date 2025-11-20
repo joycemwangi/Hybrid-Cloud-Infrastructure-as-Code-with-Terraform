11af7082189e2aefdf8b98e8dbeb2f0b94e75c83
Joyce Mwangi <44641045+joycemwangi@users.noreply.github.com>
2025-11-20 05:15:00 +0300

    Update variables.tf

--- modules/panorama-access/variables.tf
+++ modules/panorama-access/variables.tf
@@ -1,1 +1,17 @@
+variable "admin_username" {
+  description = "Panorama admin username to create"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Panorama admin password (sourced from AWS Secrets Manager)"
+  type        = string
+  sensitive   = true
+}
+
+variable "admin_role" {
+  description = "Panorama role for the admin user (superuser, deviceadmin, vsysadmin...)"
+  type        = string
+  default     = "superuser"
+}
 
