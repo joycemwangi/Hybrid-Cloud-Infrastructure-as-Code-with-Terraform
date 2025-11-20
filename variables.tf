31d2ed7d7faca99db4e206e0a6b90624e303195b
Joyce Mwangi <44641045+joycemwangi@users.noreply.github.com>
2025-11-20 22:34:38 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,6 +9,30 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "aws_vpc_cidr" {
+  description = "CIDR block for the AWS VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "aws_public_subnet_cidr" {
+  description = "CIDR block for the public subnet in the AWS VPC"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "aws_public_subnet_az" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "project_name" {
+  description = "Project name tag prefix for AWS resources"
+  type        = string
+  default     = "multivendor-demo"
+}
+
 # GCP
 variable "gcp_project" {
   description = "GCP Project ID"
@@ -74,11 +98,25 @@ variable "infoblox_username" {
 variable "infoblox_password" {
   type        = string
   description = "Infoblox password"
+  sensitive   = true
 }
 
 variable "infoblox_server" {
   type        = string
-  description = "Infoblox server URL"
+  description = "Infoblox server URL or IP"
+}
+
+# Used by modules/infoblox
+variable "infoblox_hostname" {
+  description = "Hostname to register in Infoblox"
+  type        = string
+  default     = "demo-host.example.local"
+}
+
+variable "infoblox_ipv4addr" {
+  description = "IPv4 address to associate with the Infoblox host record"
+  type        = string
+  default     = "192.0.2.10"
 }
 
 ##############################################
@@ -87,23 +125,28 @@ variable "infoblox_server" {
 
 variable "f5_address" {
   type        = string
-  description = "F5 management IP"
+  description = "F5 management IP or hostname"
 }
 
 variable "f5_username" {
   type        = string
+  description = "F5 username"
 }
 
 variable "f5_password" {
   type        = string
+  description = "F5 password"
+  sensitive   = true
 }
 
 variable "f5_vs_name" {
   type        = string
+  description = "F5 Virtual Server name"
 }
 
 variable "f5_destination" {
   type        = string
+  description = "F5 Virtual Server destination IP:port"
 }
 
 ##############################################
@@ -111,20 +154,25 @@ variable "f5_destination" {
 ##############################################
 
 variable "zscaler_username" {
-  type = string
+  type        = string
+  description = "Zscaler API username"
 }
 
 variable "zscaler_password" {
-  type = string
+  type        = string
+  description = "Zscaler API password"
+  sensitive   = true
 }
 
 variable "zscaler_api_key" {
-  type = string
+  type        = string
+  description = "Zscaler API key"
+  sensitive   = true
 }
 
 variable "zscaler_rule_name" {
   type        = string
-  description = "Zscaler rule name"
+  description = "Zscaler firewall filtering rule name"
 }
 
 ##############################################
@@ -141,15 +189,19 @@ variable "tenant_name" {
 ##############################################
 
 variable "cisco_device_ip" {
-  type = string
+  type        = string
+  description = "Cisco IOS device management IP"
 }
 
 variable "cisco_username" {
-  type = string
+  type        = string
+  description = "Cisco IOS username"
 }
 
 variable "cisco_password" {
-  type = string
+  type        = string
+  description = "Cisco IOS password"
+  sensitive   = true
 }
 
 ##############################################
@@ -157,30 +209,37 @@ variable "cisco_password" {
 ##############################################
 
 variable "juniper_device_ip" {
-  type = string
+  type        = string
+  description = "Juniper device management IP"
 }
 
 variable "juniper_username" {
-  type = string
+  type        = string
+  description = "Juniper username"
 }
 
 variable "juniper_password" {
-  type = string
+  type        = string
+  description = "Juniper password"
+  sensitive   = true
 }
 
 variable "juniper_hostname" {
-  type    = string
-  default = "juniper-device"
+  type        = string
+  description = "Hostname to configure on Juniper device"
+  default     = "juniper-device"
 }
 
 variable "juniper_loopback_ip" {
-  type    = string
-  default = "10.10.10.1/32"
+  type        = string
+  description = "Loopback interface IP (CIDR) on Juniper"
+  default     = "10.10.10.1/32"
 }
 
 variable "juniper_asn" {
-  type    = number
-  default = 65000
+  type        = number
+  description = "BGP ASN for Juniper device"
+  default     = 65000
 }
 
 ##############################################
@@ -188,21 +247,25 @@ variable "juniper_asn" {
 ##############################################
 
 variable "paloalto_object_name" {
-  type = string
+  type        = string
+  description = "Palo Alto address object name"
 }
 
 variable "paloalto_object_value" {
-  type = string
+  type        = string
+  description = "Palo Alto address object value (IP/CIDR/FQDN)"
 }
 
 variable "paloalto_object_type" {
-  type    = string
-  default = "ip-netmask"
+  type        = string
+  description = "Palo Alto address object type"
+  default     = "ip-netmask"
 }
 
 variable "paloalto_tags" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  description = "Tags to attach to Palo Alto address object"
+  default     = []
 }
 
 ##############################################
@@ -210,11 +273,13 @@ variable "paloalto_tags" {
 ##############################################
 
 variable "fortinet_addr_name" {
-  type = string
+  type        = string
+  description = "Fortinet address object name"
 }
 
 variable "fortinet_subnet" {
-  type = string
+  type        = string
+  description = "Fortinet address object subnet (CIDR)"
 }
 
 ##############################################
@@ -222,7 +287,8 @@ variable "fortinet_subnet" {
 ##############################################
 
 variable "site_name" {
-  type = string
+  type        = string
+  description = "Cisco SD-Access site name"
 }
 
 ##############################################
@@ -230,20 +296,24 @@ variable "site_name" {
 ##############################################
 
 variable "panorama_admin_username" {
-  type = string
+  type        = string
+  description = "Panorama admin username"
 }
 
 variable "panorama_admin_password" {
-  type = string
+  type        = string
+  description = "Panorama admin password"
+  sensitive   = true
 }
 
 variable "panorama_admin_role" {
-  type    = string
-  default = "superuser"
+  type        = string
+  description = "Panorama admin role"
+  default     = "superuser"
 }
 
 ##############################################
-# Check Point (Fixes your validation error)
+# Check Point
 ##############################################
 
 variable "host_name" {
