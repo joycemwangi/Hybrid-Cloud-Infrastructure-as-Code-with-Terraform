8e99f728a3fc552cd63192e13ac75cd73442a7b1
Joyce Mwangi <44641045+joycemwangi@users.noreply.github.com>
2025-11-20 19:00:59 +0300

    Create main.tf

---
+++ modules/aws/main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(
+    {
+      Name = "${var.project_name}-vpc"
+    },
+    var.tags
+  )
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.public_subnet_az
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    {
+      Name = "${var.project_name}-public-subnet"
+    },
+    var.tags
+  )
+}
